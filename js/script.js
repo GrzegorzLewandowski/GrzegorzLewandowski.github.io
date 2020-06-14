ee80568ad4307a2ea8a92d012680bd3a59cf15d5
GrzegorzLewandowski <66913565+GrzegorzLewandowski@users.noreply.github.com>
2020-06-14 17:22:44 +0200

    strona

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "40%";
+  document.getElementById("main").style.marginLeft = "10%";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "10%";
+}
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNava() {
+  document.getElementById("mySidebara").style.width = "40%";
+  document.getElementById("main").style.marginLeft = "10%";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNava() {
+  document.getElementById("mySidebara").style.width = "0";
+  document.getElementById("main").style.marginLeft = "10%";
+}
